2651eb7f0ee16b9b3e16d4e940f6e6d7049ab55c
Bawantha <bawanthapasqual@gmail.com>
2024-11-28 11:10:57 +0530

    Landing PAge

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../common/NavBar";
+import "../../assets/css/LandingPage.css";
 
 function LandingPage() {
   return (
-    <div className='landingPage-main'>
-      
+    <div className="landingPage-main">
+      <NavBar />
+      <div className="landingPage-con">
+        <div className="landingPage-text">
+          <h1>
+            Book is a <br /> window to the world
+          </h1>
+          <p>wake up your dream by reading a book every day</p>
+          <button>Read Now</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../../assets/css/Navbar.css"
+
+function NavBar() {
+  return (
+    <div className='navbar-main'>
+      <div className='navbar-left'>
+          <h1>Review</h1><h2>Book</h2>  
+      </div>
+      <div className='navbar-mid'>
+        <p>Home</p>
+        <p>Category</p>
+        <p>All Books</p>
+        <p>Blogs</p>
+      </div>
+      <div className='navbar-right'>
+        <h1>Guest</h1>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
